3a9fc6712581059fe235910a905a8199a506d31a
Mark Chavez <markchav3z@gmail.com>
2016-02-26 15:15:49 +0800

    improve layout by applying masonry and add model product

---
+++ db/migrate/20160226062547_create_product.rb
@@ -0,0 +1,9 @@
+class CreateProduct < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.column :name, :string
+      t.column :description, :text
+      t.column :price, :decimal
+    end
+  end
+end
