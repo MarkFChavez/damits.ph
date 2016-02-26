d833421a7653f01313762bae793983f11880fd12
Mark Chavez <markchav3z@gmail.com>
2016-02-26 21:18:34 +0800

    setup cart page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, :price, presence: true
-  has_attached_file :photo
+  has_attached_file :photo, :styles => { medium: "300x300>", small: "150x150>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  get "/cart" => "carts#show", as: :cart
 end
