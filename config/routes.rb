ddc61d3be00a97dfe503e5aac45dc81c674e8b1a
Mark <markchav3z@gmail.com>
2016-03-30 06:41:48 +0800

    `/checkout` for better and friendly new order URL.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "/cart" => "carts#show", as: :cart
-  get "/checkout" => "checkout#new", as: :checkout
+  get "/checkout" => "orders#new", as: :checkout
   get "/order/success" => "orders/success#show", as: :order_success
   patch "/cart/add/:id" => "carts#update", as: :add_to_cart
 
